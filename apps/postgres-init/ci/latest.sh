9a1a4af8d0d31430dbb19b8626227e886e52713e
Cyrus Ho <hello@cyrusho.me>
2023-11-01 03:01:55 +0800

    Fix version postgresql-init

--- apps/postgres-init/ci/latest.sh
+++ apps/postgres-init/ci/latest.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
-version=$(curl -sX GET "https://pkgs.alpinelinux.org/packages?name=postgresql16-client&branch=edge&arch" | grep -oP '(?<=<td class="version">)[^<]*' 2>/dev/null)
-version="${version%%_*}"
-version="${version%%-*}"
-printf "%s" "${version}"
+
+echo "16.0-r1"
+
